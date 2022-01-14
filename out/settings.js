759c9990f09af01148ec175f277aeb6dc400673c
spinearr <spinear@gmail.com>
2022-01-14 17:08:50 +0900

    리스트 중복 땜에 버그 생긴거 찾음! 망할

--- out/settings.js
+++ out/settings.js
@@ -11,25 +11,25 @@ export const boughtServerHackingTarget = "joesguns";
 export const boughtServerRam = 16;
 
 export const advHackingTarget = [
-    "n00dles", "harakiri-sushi", "phantasy", "omega-net", 
+    "n00dles", "harakiri-sushi", "phantasy", "omega-net",
     "the-hub", "catalyst", "rho-construction", "global-pharm", "4sigma"
 ];
 
 export const serverList = [
     //factions
     "CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z",
-    "n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea",  
-    "harakiri-sushi", "neo-net", "zer0", "max-hardware", "iron-gym",   
-    "phantasy", "silver-helix", "omega-net", "crush-fitness", "johnson-ortho",  
-    "the-hub", "comptek", "netlink", "catalyst", "rothman-uni", "aevum-police", 
-    "summit-uni", "rho-construction", "millenium-fitness", "alpha-ent", 
-    "syscore", "lexo-corp", "zb-institute", "snap-fitness",    
-    "zb-def", "unitalife", "solaris", "global-pharm", "univ-energy", "zeus-med", "nova-med",   
-    "galactic-cyber", "taiyang-digital", "deltaone", "aerocorp", 
-    "icarus", "omnia", "infocomm", "defcomm", "titan-labs", "helios", 
-    "stormtech", "4sigma", "b-and-a", "fulcrumtech", "omnitek", "blade", "clarkinc",
-    "kuai-gong", "fulcrumassets", "blade", "nwo", "ecorp",   
-    "vitalife", "The-Cave", ".", "megacorp", "microdyne", 
+    "n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea",
+    "harakiri-sushi", "neo-net", "zer0", "max-hardware", "iron-gym",
+    "phantasy", "silver-helix", "omega-net", "crush-fitness", "johnson-ortho",
+    "the-hub", "comptek", "netlink", "catalyst", "rothman-uni", "aevum-police",
+    "summit-uni", "rho-construction", "millenium-fitness", "alpha-ent",
+    "syscore", "lexo-corp", "zb-institute", "snap-fitness",
+    "zb-def", "unitalife", "solaris", "global-pharm", "univ-energy", "zeus-med", "nova-med",
+    "galactic-cyber", "taiyang-digital", "deltaone", "aerocorp",
+    "icarus", "omnia", "infocomm", "defcomm", "titan-labs", "helios",
+    "stormtech", "4sigma", "b-and-a", "fulcrumtech", "omnitek", "clarkinc",
+    "kuai-gong", "fulcrumassets", "blade", "nwo", "ecorp",
+    "vitalife", "The-Cave", ".", "megacorp", "microdyne",
     "powerhouse-fitness", "applied-energetics"
 ];
 
