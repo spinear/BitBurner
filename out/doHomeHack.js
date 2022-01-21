9a36b2749502d48d1e7ea86e1bef76fa5a90ae23
spinear <spinear@gmail.com>
2022-01-22 00:04:08 +0900

    엘레강트

--- out/doHomeHack.js
+++ out/doHomeHack.js
@@ -3,28 +3,30 @@ import { loopHackFileName } from "./settings.js";
 
 /** @type import(".").NS */
 let ns = null;
+let host = 'home';
 
 export async function main(_ns) {
 	ns = _ns;
-	let host = "home";
-	let target = ns.peek(2);
 	let isSmushed = ns.peek(3);
-
 	let calcedThreads = calcThreads(ns, host, loopHackFileName.vWeaken);
 
 	if (isSmushed == 'true') {
 		ns.tprint(`집에 타겟 변경으로 스크립트 재실행!`)
-		killHackScripts(ns, 'home');
-		calcedThreads = calcThreads(ns, host, loopHackFileName.vWeaken);
-		runLoopHack(ns, loopHackFileName, host, calcedThreads, target, 1);
+		restartHomeScript(ns);
 		return;
 	}
 
 	if ((calcedThreads.remainingRam / calcedThreads.maxRam * 100) > 30) {
 		ns.tprint(`집에 램 증가로 스크립트 재실행!`)
-		killHackScripts(ns, 'home');
-		calcedThreads = calcThreads(ns, host, loopHackFileName.vWeaken);
-		runLoopHack(ns, loopHackFileName, host, calcedThreads, target, 1);
+		restartHomeScript(ns);
 		return;
 	}
+}
+
+function restartHomeScript(_ns) {
+	ns = _ns;
+	let target = ns.peek(2);
+	killHackScripts(ns, 'home');
+	let calcedThreads = calcThreads(ns, host, loopHackFileName.vWeaken);
+	runLoopHack(ns, loopHackFileName, host, calcedThreads, target, 1);
 }
