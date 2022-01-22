a81aaacec9cfa30cb37c33a20994e4977b062f2f
spinear <spinear@gmail.com>
2022-01-22 15:14:17 +0900

    따옴표 바꿈

--- out/settings.js
+++ out/settings.js
@@ -1,19 +1,19 @@
 /** @param {NS} ns **/
 
 export const factionServers = [
-    "CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z"
+    'CSEC', 'avmnite-02h', 'I.I.I.I', 'run4theh111z'
 ];
 
 // ----------------------------------------------------------------
 // 이 어레이는 인덱스끼리 관계가 서로 일치 해야함!
 export const factionList = [
-    "CyberSec", // 0
-    "NiteSec", // 1
-    "Chongqing", // 2
+    'CyberSec', // 0
+    'NiteSec', // 1
+    'Chongqing', // 2
     'Tian Di Hui', // 3
-    "The Black Hand", // 4
-    "BitRunners", // 5
-    "Daedalus" // 6
+    'The Black Hand', // 4
+    'BitRunners', // 5
+    'Daedalus' // 6
 ];
 
 export const augList = [
@@ -35,33 +35,33 @@ export const repCost = [
 ];
 // ----------------------------------------------------------------
 export const loopHackFileName = {
-    vWeaken: "weaken.js",
-    vGrow: "grow.js",
-    vHack: "hack.js"
+    vWeaken: 'weaken.js',
+    vGrow: 'grow.js',
+    vHack: 'hack.js'
 };
 
 export const advHackingTarget = [
-    "n00dles", "harakiri-sushi", "phantasy", "omega-net",
-    "the-hub", "catalyst", "rho-construction", "global-pharm", "b-and-a", "megacorp"
+    'n00dles', 'harakiri-sushi', 'phantasy', 'omega-net',
+    'the-hub', 'catalyst', 'rho-construction', 'global-pharm', 'b-and-a', 'megacorp'
 ];
 
 
 export const serverList = [
     //factions
-    "CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "w0r1d_d43m0n",
+    'CSEC', 'avmnite-02h', 'I.I.I.I', 'run4theh111z', 'w0r1d_d43m0n',
     //normals
-    "n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea",
-    "harakiri-sushi", "neo-net", "zer0", "max-hardware", "iron-gym",
-    "phantasy", "silver-helix", "omega-net", "crush-fitness", "johnson-ortho",
-    "the-hub", "comptek", "netlink", "catalyst", "rothman-uni", "aevum-police",
-    "summit-uni", "rho-construction", "millenium-fitness", "alpha-ent",
-    "syscore", "lexo-corp", "zb-institute", "snap-fitness",
-    "zb-def", "unitalife", "solaris", "global-pharm", "univ-energy", "zeus-med", "nova-med",
-    "galactic-cyber", "taiyang-digital", "deltaone", "aerocorp",
-    "icarus", "omnia", "infocomm", "defcomm", "titan-labs", "helios",
-    "stormtech", "4sigma", "b-and-a", "fulcrumtech", "omnitek", "clarkinc",
-    "kuai-gong", "fulcrumassets", "blade", "nwo", "ecorp",
-    "vitalife", "The-Cave", ".", "megacorp", "microdyne",
-    "powerhouse-fitness", "applied-energetics"
+    'n00dles', 'foodnstuff', 'sigma-cosmetics', 'joesguns', 'nectar-net', 'hong-fang-tea',
+    'harakiri-sushi', 'neo-net', 'zer0', 'max-hardware', 'iron-gym',
+    'phantasy', 'silver-helix', 'omega-net', 'crush-fitness', 'johnson-ortho',
+    'the-hub', 'comptek', 'netlink', 'catalyst', 'rothman-uni', 'aevum-police',
+    'summit-uni', 'rho-construction', 'millenium-fitness', 'alpha-ent',
+    'syscore', 'lexo-corp', 'zb-institute', 'snap-fitness',
+    'zb-def', 'unitalife', 'solaris', 'global-pharm', 'univ-energy', 'zeus-med', 'nova-med',
+    'galactic-cyber', 'taiyang-digital', 'deltaone', 'aerocorp',
+    'icarus', 'omnia', 'infocomm', 'defcomm', 'titan-labs', 'helios',
+    'stormtech', '4sigma', 'b-and-a', 'fulcrumtech', 'omnitek', 'clarkinc',
+    'kuai-gong', 'fulcrumassets', 'blade', 'nwo', 'ecorp',
+    'vitalife', 'The-Cave', '.', 'megacorp', 'microdyne',
+    'powerhouse-fitness', 'applied-energetics'
 ];
 
