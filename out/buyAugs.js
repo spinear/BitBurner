69c8f14fa1520c221210ec12b936ff3759595dcb
spinear <spinear@gmail.com>
2022-01-24 16:12:39 +0900

    잘못씀!

--- out/buyAugs.js
+++ out/buyAugs.js
@@ -8,12 +8,13 @@ export async function main(_ns) {
     //if (ns.getServerMaxRam('home') < 512) return;
     fwo.factionList.forEach(flist => {
         const augs = ns.getAugmentationsFromFaction(flist);
-        for (let aug of augs) {
+        augs.forEach(aug => {
             const repCost = ns.getAugmentationRepReq(aug);
             const moneyCost = ns.getAugmentationPrice(aug);
-            if (ns.getFactionRep(flist) > repCost && ns.getServerMaxMoney('home') > moneyCost) {
+            if (ns.getFactionRep(flist) > repCost
+                && ns.getServerMoneyAvailable('home') * 0.5 > moneyCost) {
                 ns.purchaseAugmentation(aug);
             }
-        }
+        });
     });
 }
